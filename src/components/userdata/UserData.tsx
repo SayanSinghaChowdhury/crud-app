b3187999b700fc8c33cc5b0735500e8af27e6a40
Sayan Singha Chowdhury <sayansinghachowdhury.dev@gmail.com>
2026-06-26 19:48:42 +0530

    react hook form use

--- src/components/userdata/UserData.tsx
+++ src/components/userdata/UserData.tsx
@@ -1,5 +1,23 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../shadcnui/card";
+
 const UserData = () => {
-  return <div>Enter</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle></CardTitle>
+      </CardHeader>
+      <CardContent></CardContent>
+      <CardFooter></CardFooter>
+    </Card>
+  );
 };
 
 export default UserData;
