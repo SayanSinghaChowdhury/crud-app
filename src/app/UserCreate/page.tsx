9147dd8495103cb636905fd5399165bd7985b08a
Sayan Singha Chowdhury <sayansinghachowdhury.dev@gmail.com>
2026-06-26 19:40:00 +0530

    project clear and basic components added

---
+++ src/app/UserCreate/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <section className="grid h-dvh place-items-center">Create</section>;
+};
+
+export default page;
---
+++ src/components/userdata/UserData.tsx
@@ -0,0 +1,5 @@
+const UserData = () => {
+  return <div>Enter</div>;
+};
+
+export default UserData;
