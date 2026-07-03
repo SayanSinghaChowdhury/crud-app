b9509ab131a2dc03c6fb58cc8d5b0c01029552dc
Sayan Singha Chowdhury <sayansinghachowdhury.dev@gmail.com>
2026-07-03 13:50:32 +0530

    ui update

--- src/components/userdata/DeleteUser.tsx
+++ src/components/userdata/DeleteUser.tsx
@@ -24,7 +24,7 @@ const DeleteUser = () => {
       variant={"destructive"}>
       {remove ?
         <>
-          Deliting
+          Deleting
           <BrushCleaningIcon />
         </>
       : <>
