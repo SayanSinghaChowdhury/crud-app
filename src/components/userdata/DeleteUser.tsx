fe5ed657765277ac8b01ab37cd3f05a1074c92de
Sayan Singha Chowdhury <sayansinghachowdhury.dev@gmail.com>
2026-08-04 14:50:55 +0530

    debugging the delet component

--- src/components/userdata/DeleteUser.tsx
+++ src/components/userdata/DeleteUser.tsx
@@ -13,6 +13,8 @@ const DeleteUser = ({ usDelete }: userDeleteProcess) => {
   const [remove, setRemove] = useState(false);
 
   const HandleClear = async () => {
+    // server to client
+
     const { issuccess, message } = await userDeleteAction(usDelete);
     setRemove(true);
 
@@ -21,9 +23,6 @@ const DeleteUser = ({ usDelete }: userDeleteProcess) => {
     } else {
       toast.error(message);
     }
-
-    // server to client
-    await userDeleteAction(usDelete);
 
     await new Promise((r) => {
       setTimeout(r, 500);
